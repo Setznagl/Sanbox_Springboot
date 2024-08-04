83f92bbec3bd46b4615aa081338bed3b1d8ed3f0
Gabriel Setznagl <gabrielembraer3@gmail.com>
2024-08-03 21:24:29 -0300

    build(New Files): Aula 02
    Intrudução a API's Restfull

--- Aula02/src/main/java/com/example/aula02/Model/Pessoa.java
+++ Aula02/src/main/java/com/example/aula02/Model/Pessoa.java
@@ -3,21 +3,20 @@ package com.example.aula02.Model;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.Data;
 
+//Anotações do Lombok e JPA
 @Data
 @Entity
-@Table(name = "Aula02Pessoa")
 public class Pessoa {
 
     @Id
     private Long id;
 
-    @Column(unique = false, length = 10, nullable = false)
+    @Column(length = 10, nullable = false)
     private String nome;
 
-    @Column(unique = false, length = 3, nullable = false)
+    @Column(length = 3, nullable = false)
     private int idade;
 
 }
